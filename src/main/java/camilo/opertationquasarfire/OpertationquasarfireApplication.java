f97bd71322680561fe1f1e431e87b3e8369bb9f3
CamiloSotoC <camilo.soto@usach.cl>
2024-08-25 04:51:02 -0400

    Update OpertationquasarfireApplication.java

--- src/main/java/camilo/opertationquasarfire/OpertationquasarfireApplication.java
+++ src/main/java/camilo/opertationquasarfire/OpertationquasarfireApplication.java
@@ -3,7 +3,6 @@ package camilo.opertationquasarfire;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 
@@ -18,7 +17,9 @@ public class OpertationquasarfireApplication {
 	public OpenAPI customOpenAPI() {
 		return new OpenAPI()
 				.info(new Info()
-						.title("Operation Quasar Fire - API")
-						.description("API Operation Quasar Fire"));
+						.title("API - Operation Quasar Fire")
+						.description("This repository contains the implementation of the 'Operation Quasar Fire' challenge in Java with SpringBoot."
+						+"<br>"
+						+"The goal is to create a program that returns the location and rebuild the distress message of an Imperial cargo ship adrift in an asteroid field."));
 	}
 }
