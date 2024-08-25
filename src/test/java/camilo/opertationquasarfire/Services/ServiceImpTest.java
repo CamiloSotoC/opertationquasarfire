cd0ffd7a5b5362a516b6bb1992b037233c3018d3
CamiloSotoC <camilo.soto@usach.cl>
2024-08-25 14:59:16 -0400

    Update ServiceImpTest.java

--- src/test/java/camilo/opertationquasarfire/Services/ServiceImpTest.java
+++ src/test/java/camilo/opertationquasarfire/Services/ServiceImpTest.java
@@ -2,17 +2,22 @@ package camilo.opertationquasarfire.Services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
 
+import org.apache.commons.lang3.ObjectUtils.Null;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import camilo.opertationquasarfire.Exceptions.InformationException;
+import camilo.opertationquasarfire.Exceptions.ResquestException;
 import camilo.opertationquasarfire.Models.Position;
 import camilo.opertationquasarfire.Models.Satellite;
 import camilo.opertationquasarfire.Models.SatelliteRequest;
@@ -61,8 +66,14 @@ public class ServiceImpTest {
                 new Satellite("kenobi", new Position(-500, -200)),
                 new Satellite("skywalker", new Position(100, -100)),
                 new Satellite("sato", new Position(500, 100)));
+        List<Satellite> satellites2 = List.of(
+                new Satellite("kenobi", new Position(-500, -200)),
+                new Satellite("skywalker", new Position(100, -100)),
+                new Satellite("sato", new Position(500, 100)));
         when(repository.getSatellites()).thenReturn(satellites);
         List<Satellite> result = service.getSatellites();
+        assertNotNull(result);
+        ;
         assertEquals(satellites, result);
     }
 
@@ -97,18 +108,16 @@ public class ServiceImpTest {
     @Test
     void testGetSpaceshipData2() {
         List<Satellite> satellites = List.of(
-                new Satellite("kenobi", new Position(-500, -200), 100.0, List.of("este", "", "", "mensaje", "") ),
-                new Satellite("skywalker", new Position(100, -100), 115.5, List.of("", "es", "", "", "secreto") ),
-                new Satellite("sato", new Position(500, 100), 142.7, List.of("este", "", "un", "", "") ));
-        List<Double> distances = List.of(satellites.get(0).getDistance(), satellites.get(1).getDistance(), satellites.get(2).getDistance());
+                new Satellite("kenobi", new Position(-500, -200), 100.0, List.of("este", "", "", "mensaje", "")),
+                new Satellite("skywalker", new Position(100, -100), 115.5, List.of("", "es", "", "", "secreto")),
+                new Satellite("sato", new Position(500, 100), 142.7, List.of("este", "", "un", "", "")));
+        List<Double> distances = List.of(satellites.get(0).getDistance(), satellites.get(1).getDistance(),
+                satellites.get(2).getDistance());
         List<List<String>> messages = List.of(
-            satellites.get(0).getMessage(),
-            satellites.get(1).getMessage(),
-            satellites.get(2).getMessage());
+                satellites.get(0).getMessage(),
+                satellites.get(1).getMessage(),
+                satellites.get(2).getMessage());
         when(repository.getSatellites()).thenReturn(satellites);
-        when(repository.getSatelliteByName("kenobi")).thenReturn(satellites.get(0));
-        when(repository.getSatelliteByName("skywalker")).thenReturn(satellites.get(1));
-        when(repository.getSatelliteByName("sato")).thenReturn(satellites.get(2));
         when(repository.getDistances()).thenReturn(distances);
         when(repository.getMessages()).thenReturn(messages);
         SpaceshipResponse result = service.getSpaceshipData();
@@ -116,11 +125,106 @@ public class ServiceImpTest {
         assertEquals(-58.315252587138595, result.getPosition().getX());
         assertEquals(-69.55141837312165, result.getPosition().getY());
         assertEquals("este es un mensaje secreto", result.getMessage());
-
     }
 
     @Test
     void testSetSatellite() {
+        Satellite satellite = new Satellite("kenobi", new Position(-500, -200));
+        SatelliteRequest request = new SatelliteRequest("kenobi", 100.0, List.of("este", "", "", "mensaje", ""));
+        when(repository.getSatelliteByName("kenobi")).thenReturn(satellite);
+        Satellite result = service.setSatellite("kenobi", request);
+        assertNotNull(result);
+        assertEquals(100.0, result.getDistance());
+        assertEquals(List.of("este", "", "", "mensaje", ""), result.getMessage());
+    }
 
+    @Test
+    void testExceptionRQE1() {
+        List<SatelliteRequest> satellitesRequest = List.of(
+                new SatelliteRequest("kenobi", 100.0, List.of("este", "", "", "mensaje", "")),
+                new SatelliteRequest("skywalker", 115.5, List.of("", "es", "", "", "secreto")));
+        ResquestException e = assertThrows(
+                ResquestException.class,
+                () -> service.getSpaceshipData(satellitesRequest));
+        assertEquals("RQE1", e.getCode());
+        assertEquals("There is not enough information from satellites.", e.getMessage());
+    }
+
+    @Test
+    void testExceptionRQE2() {
+        List<SatelliteRequest> satellitesRequest = List.of(
+                new SatelliteRequest(null, 100.0, List.of("este", "", "", "mensaje", "")),
+                new SatelliteRequest("skywalker", 115.5, List.of("", "es", "", "", "secreto")),
+                new SatelliteRequest("sato", 142.7, List.of("este", "", "un", "", "")));
+        ResquestException e = assertThrows(
+                ResquestException.class,
+                () -> service.getSpaceshipData(satellitesRequest));
+        assertEquals("RQE2", e.getCode());
+        assertEquals("There is not enough information from satellites.", e.getMessage());
+    }
+
+    @Test
+    void testExceptionRQE3() {
+        List<SatelliteRequest> satellitesRequest = List.of(
+                new SatelliteRequest("darthvader", 100.0, List.of("este", "", "", "mensaje", "")),
+                new SatelliteRequest("skywalker", 115.5, List.of("", "es", "", "", "secreto")),
+                new SatelliteRequest("sato", 142.7, List.of("este", "", "un", "", "")));
+        ResquestException e = assertThrows(
+                ResquestException.class,
+                () -> service.getSpaceshipData(satellitesRequest));
+        assertEquals("RQE3", e.getCode());
+        assertEquals("That satellite does not exist: 'darthvader'.", e.getMessage());
+    }
+
+    @Test
+    void testExceptionRQE4() {
+        List<SatelliteRequest> satellitesRequest = List.of(
+                new SatelliteRequest("kenobi", 100.0, null),
+                new SatelliteRequest("skywalker", 115.5, List.of("", "es", "", "", "secreto")),
+                new SatelliteRequest("sato", 142.7, List.of("este", "", "un", "", "")));
+        Satellite satellite = new Satellite("kenobi", new Position(-500, -200));
+        when(repository.getSatelliteByName("kenobi")).thenReturn(satellite);
+        ResquestException e = assertThrows(
+                ResquestException.class,
+                () -> service.getSpaceshipData(satellitesRequest));
+        assertEquals("RQE4", e.getCode());
+        assertEquals("There is not enough information from satellite: 'kenobi'.", e.getMessage());
+    }
+
+    @Test
+    void testExceptionINE1() {
+        List<Satellite> satellites = List.of(
+                new Satellite("kenobi", new Position(-500, -200), 100.0, List.of("este", "", "", "mensaje", "")),
+                new Satellite("skywalker", new Position(100, -100), 115.5, List.of("", "es", "", "", "secreto")));
+        when(repository.getSatellites()).thenReturn(satellites);      
+        InformationException e = assertThrows(
+            InformationException.class,
+                () -> service.getSpaceshipData());
+        assertEquals("INE1", e.getCode());
+        assertEquals("There is not enough information from satellites.", e.getMessage());
+    }
+    @Test
+    void testExceptionINE2() {
+        List<Satellite> satellites = List.of(
+                new Satellite("kenobi", new Position(-500, -200), 100.0, null),
+                new Satellite("skywalker", new Position(100, -100), 115.5, List.of("", "es", "", "", "secreto")),
+                new Satellite("sato", new Position(500, 100), 142.7, List.of("este", "", "un", "", "")));
+        when(repository.getSatellites()).thenReturn(satellites);    
+        InformationException e = assertThrows(
+            InformationException.class,
+                () -> service.getSpaceshipData());
+        assertEquals("INE2", e.getCode());
+        assertEquals("There is not enough information from satellite: 'kenobi'.", e.getMessage());
+    }
+
+    @Test
+    void testExceptionINE3() {
+        SatelliteRequest request = new SatelliteRequest( 100.0, List.of("este", "", "", "mensaje", ""));
+        when(repository.getSatelliteByName("darthvader")).thenReturn(null);  
+        InformationException e = assertThrows(
+            InformationException.class,
+                () -> service.setSatellite("darthvader", request));
+        assertEquals("INE3", e.getCode());
+        assertEquals("That satellite does not exist: 'darthvader'.", e.getMessage());
     }
 }
