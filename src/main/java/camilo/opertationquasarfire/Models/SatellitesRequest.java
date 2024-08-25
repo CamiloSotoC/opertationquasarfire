1dcfecb5836933d8eb7a97ef77b97511b981e91f
CamiloSotoC <camilo.soto@usach.cl>
2024-08-25 04:50:39 -0400

    Update SatellitesRequest.java

--- src/main/java/camilo/opertationquasarfire/Models/SatellitesRequest.java
+++ src/main/java/camilo/opertationquasarfire/Models/SatellitesRequest.java
@@ -1,10 +1,11 @@
 package camilo.opertationquasarfire.Models;
 
 import java.util.List;
-
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class SatellitesRequest {
     private List<SatelliteRequest> satellites;
 }
