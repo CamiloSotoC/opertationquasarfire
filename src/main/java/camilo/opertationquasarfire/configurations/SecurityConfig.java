791186d52142ca875ead206bdfdfed234d138f9f
CamiloSotoC <camilo.soto@usach.cl>
2024-09-02 18:56:35 -0400

    Update SecurityConfig.java

--- src/main/java/camilo/opertationquasarfire/configurations/SecurityConfig.java
+++ src/main/java/camilo/opertationquasarfire/configurations/SecurityConfig.java
@@ -2,11 +2,16 @@ package camilo.opertationquasarfire.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.session.SessionRegistry;
-import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,27 +19,31 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .authorizeHttpRequests(authorize -> {
-                    //authorize.requestMatchers("/").permitAll();
+                    // authorize.requestMatchers("/").permitAll();
                     authorize.anyRequest().authenticated();
                 })
-                /*.formLogin(login -> {
-                    login.successHandler(this.succesHanlder());
-                    login.permitAll();
-                })*/
+                /*
+                 * .formLogin(login -> {
+                 * login.successHandler(this.succesHanlder());
+                 * login.permitAll();
+                 * })
+                 */
                 .sessionManagement(management -> {
                     management
-                            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);// ALWAYS - IF_REQUIRED - NEVER - STATELESS
+                            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);// ALWAYS - IF_REQUIRED - NEVER -
+                                                                                    // STATELESS
                     management.invalidSessionUrl("/login");
-                    //management.sessionFixation().migrateSession();// migrateSession - newSession - none
-                    //management.maximumSessions(1).sessionRegistry(sessionRegistry());
+                    // management.sessionFixation().migrateSession();// migrateSession - newSession
+                    // - none
+                    // management.maximumSessions(1).sessionRegistry(sessionRegistry());
                 })
                 .httpBasic(basic -> {
                 })
                 .csrf(csrf -> {
-                    //csrf.disable();
+                    // csrf.disable();
                     csrf.ignoringRequestMatchers("/topsecret/");
                     csrf.ignoringRequestMatchers("/topsecret_split/*");
                     csrf.ignoringRequestMatchers("/user");
@@ -43,14 +52,42 @@ public class SecurityConfig {
 
     }
 
-    /*private AuthenticationSuccessHandler succesHanlder() {
-        return ((request, response, authentication) -> response.sendRedirect(""));
-    }*/
+    /*
+     * private AuthenticationSuccessHandler succesHanlder() {
+     * return ((request, response, authentication) -> response.sendRedirect(""));
+     * }
+     */
 
     /*
-    @Bean()
-    public SessionRegistry sessionRegistry() {
-        return new SessionRegistryImpl();
-    }*/
+     * @Bean()
+     * public SessionRegistry sessionRegistry() {
+     * return new SessionRegistryImpl();
+     * }
+     */
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(
+                User.withUsername("user")
+                        .password("passwordd")
+                        .roles()
+                        .build());
+        return manager;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(HttpSecurity httpSecurity, PasswordEncoder passwordEncoder)
+            throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = httpSecurity
+                .getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder);
+        return authenticationManagerBuilder.build();
+    }
 
 }
